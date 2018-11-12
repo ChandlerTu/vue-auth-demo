6d31649040140e394a50c610f24ccf170de3f88c
Chandler Tu <chandlertu@chandlertu.com>
2018-11-12 16:08:42 +0800

    auth-flow

--- src/main.js
+++ src/main.js
@@ -3,10 +3,44 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import App from './App.vue'
+import auth from './auth'
+import App from './components/App.vue'
+import About from './components/About.vue'
+import Dashboard from './components/Dashboard.vue'
+import Login from './components/Login.vue'
 
-Vue.config.productionTip = false
+function requireAuth(to, from, next) {
+  if (!auth.loggedIn()) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
 
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/about', component: About },
+    { path: '/dashboard', component: Dashboard, beforeEnter: requireAuth },
+    { path: '/login', component: Login },
+    {
+      path: '/logout',
+      beforeEnter(to, from, next) {
+        auth.logout()
+        next('/')
+      }
+    }
+  ]
+})
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  // replace the content of <div id="app"></div> with App
+  render: h => h(App)
+})
